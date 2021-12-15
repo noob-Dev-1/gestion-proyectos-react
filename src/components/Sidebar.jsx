9bc709d31d733566daf477025e369176856fc6b0
Cristian Bustos <cristiandavid.bt@gmail.com>
2021-12-15 15:48:42 -0500

    creación de la autenticación sin funcionalidad en login y registro

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,51 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from 'context/authContext';
+import PrivateComponent from './PrivateComponent';
+import { useUser } from 'context/userContext';
 
 const SidebarLinks = () => {
   return (
     <ul className='mt-12'>
       <SidebarRoute to='' title='Inicio' icon='fas fa-home' />
-      <SidebarRoute to='/usuarios' title='Usuarios' icon='fas fa-users' />
-      <SidebarRoute to='/proyectos' title='Proyectos' icon= 'fas fa-clipboard-list' />
+      <PrivateComponent roleList={['ADMINISTRADOR']}>
+        <SidebarRoute to='/usuarios' title='Usuarios' icon='fas fa-user' />
+      </PrivateComponent>
+      <SidebarRoute to='/proyectos' title='Proyectos' icon='fas fa-smile-wink' />
+      <PrivateComponent roleList={['ADMINISTRADOR', 'LIDER']}>
+        <SidebarRoute to='/inscripciones' title='Aprobacion Inscripciones' icon='fas fa-users' />
+      </PrivateComponent>
       <SidebarRoute to='/page2' title='Pagina2' icon='fas fa-smile-wink' />
       <SidebarRoute to='/category1' title='Catego 1' icon='fab fa-amazon' />
       <SidebarRoute to='/category1/page1' title='Test' icon='fas fa-car' />
+      <Logout />
     </ul>
+  );
+};
+
+const Logout = () => {
+  const { setToken } = useAuth();
+  const deleteToken = () => {
+    console.log('eliminar token');
+    setToken(null);
+  };
+  return (
+    <li onClick={() => deleteToken()}>
+      <NavLink to='/auth/login' className='sidebar-route text-red-700'>
+        <div className='flex items-center'>
+          <i className='fas fa-sign-out-alt' />
+          <span className='text-sm  ml-2'>Cerrar Sesión</span>
+        </div>
+      </NavLink>
+    </li>
   );
 };
 
 const Logo = () => {
   return (
     <div className='py-3 w-full flex flex-col items-center justify-center'>
-      <img src='logoNoobDev.png' alt='Logo' className='h-30' />
-      <span className='my-2 text-xl font-bold text-center'>Gestión de proyectos</span>
+      <img src='logo.png' alt='Logo' className='h-16' />
+      <span className='my-2 text-xl font-bold text-center'>Título de Mi Aplicación</span>
     </div>
   );
 };
@@ -35,7 +62,7 @@ const Sidebar = () => {
           <SidebarLinks />
         </div>
       </div>
-      <div className='flex md:hidden w-full justify-between bg-red-800 p-2 text-white'>
+      <div className='flex md:hidden w-full justify-between bg-gray-800 p-2 text-white'>
         <i className={`fas fa-${open ? 'times' : 'bars'}`} onClick={() => setOpen(!open)} />
         <i className='fas fa-home' />
       </div>
@@ -68,8 +95,8 @@ const SidebarRoute = ({ to, title, icon }) => {
         to={to}
         className={({ isActive }) =>
           isActive
-            ? 'sidebar-route text-white bg-green-600'
-            : 'sidebar-route text-black hover:text-white hover:bg-green-700'
+            ? 'sidebar-route text-white bg-indigo-700'
+            : 'sidebar-route text-gray-900 hover:text-white hover:bg-indigo-400'
         }
       >
         <div className='flex items-center'>
