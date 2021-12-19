96ecfc76be5faaa6a8f61f7836846f5dd62ee911
jarol <jaancastano@utp.edu.co>
2021-12-19 13:22:21 -0500

    despliegue heroku

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -6,7 +6,7 @@ const AuthLayout = () => {
     <div className='flex flex-col md:flex-row flex-no-wrap h-screen'>
       <div className='flex w-full h-full'>
         <div className='w-full h-full  overflow-y-scroll'>
-          Layout de Autenticacion heroku
+          Layout de Autenticaciones
           <Outlet />
         </div>
       </div>
